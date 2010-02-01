86595420d5d6b304f6212c6dcf05f15b5f14a39c
Alessio Bianchi <venator85@gmail.com>
2010-02-01 11:51:01 +0100

    Improved backup_mirror_mac

--- backup/backup_mirror_mac/backup_mirror.sh
+++ backup/backup_mirror_mac/backup_mirror.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+cd /Users/venator/Tools/backup_mirror/
 ICON_PROGRESS="progress.png"
 ICON_DONE="done.png"
 ICON_ERROR="error.png"
 
 pingok=`ping -c 1 gateway 2>/dev/null`
-if [ $? -eq 0 ]; then			# controlla che siamo al negozio
-	growlnotify --image $ICON_PROGRESS -m "Backup mirroring in corso..."
-	rsync -avL --delete gateway:/home/venator/backup_sispac/curweek /Users/venator/BackupSISPAC
-	retcode=$?
+if [ $? -eq 0 ]			# controlla che siamo al negozio
+then
+	growlnotify --image $ICON_PROGRESS -m "Backup mirroring in progress..."
+	rsync -avL --delete gateway:/home/venator/backup_sispac/curweek/ /Users/venator/Tools/BackupSISPAC/
 	if [ $retcode -eq 0 ]; then
 		growlnotify --image $ICON_DONE -m "Backup mirroring completato."
 	else
@@ -17,4 +18,3 @@ if [ $? -eq 0 ]; then			# controlla che siamo al negozio
 else
 	growlnotify --image $ICON_PROGRESS -m "Backup mirroring non eseguito: non al negozio."
 fi
-
