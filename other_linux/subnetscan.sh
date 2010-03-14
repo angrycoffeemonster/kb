bf2bd65e08f3d0ada3d5f1baee7ad15cc7272e7c
Alessio Bianchi <venator85@gmail.com>
2010-03-14 15:11:09 +0100

    Added check and auto-install of NetAddr::IP perl module for subnetscan.

--- other_linux/subnetscan.sh
+++ other_linux/subnetscan.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-(./ip_address_space.pl $1 | xargs -n 1 -P 254 ping -c1 -w1) 2>/dev/null | grep icmp
+if [ $# -eq 0 ]; then
+	echo ":: Warning: network interface not provided. Assuming eth0"
+	NIC="eth0"
+else
+	NIC=$1
+fi
+
+perl -MNetAddr::IP -e 1 &>/dev/null
+if [ $? -ne 0 ]; then
+	echo ":: Perl module 'NetAddr::IP' not installed. Installing now..."
+	sudo pacman -S perl-netaddr-ip
+fi
+
+(./ip_address_space.pl $NIC | xargs -n 1 -P 254 ping -c1 -w1) 2>/dev/null | grep icmp
