1ad29ee2390a87b4414e1568d73a7e911995adeb
Alessio Bianchi <venator85@gmail.com>
2010-03-27 10:10:10 +0100

    Fixed error management in backup_mirror mac version

--- backup/backup_mirror_mac/backup_mirror.sh
+++ backup/backup_mirror_mac/backup_mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /Users/venator/Tools/backup_mirror/
+cd /Users/venator/Software/kb/backup/backup_mirror_mac
 ICON_PROGRESS="progress.png"
 ICON_DONE="done.png"
 ICON_ERROR="error.png"
@@ -10,6 +10,7 @@ if [ $? -eq 0 ]			# controlla che siamo al negozio
 then
 	growlnotify --image $ICON_PROGRESS -m "Backup mirroring in progress..."
 	rsync -avL --delete gateway:/home/venator/backup_sispac/curweek/ /Users/venator/Tools/BackupSISPAC/
+	retcode=$?
 	if [ $retcode -eq 0 ]; then
 		growlnotify --image $ICON_DONE -m "Backup mirroring completato."
 	else
