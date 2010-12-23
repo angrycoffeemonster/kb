6db77003b6ad8690b552233a971f434872d47868
Alessio Bianchi <venator85@gmail.com>
2010-12-23 20:07:41 +0100

    updated extract_magnet_links.py to support urls

--- other_linux/extract_magnet_links.py
+++ other_linux/extract_magnet_links.py
@@ -7,10 +7,11 @@ Extracts magnet links from .html files (tested with eztv.it)
 import re
 import os
 import sys
+import urllib2
 
 magnet_re = re.compile("href=\"(magnet:.+?)\"", re.I)
 
-f = open(sys.argv[1], 'r')
+f = urllib2.urlopen(sys.argv[1])
 lines = f.readlines()
 
 for l in lines:
