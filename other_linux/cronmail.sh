a4d1d959a0edae18fa1f8055e40fc474a2c470a0
Alessio Bianchi <venator85@gmail.com>
2012-09-02 17:08:52 +0200

    Cronmail now can replace sendmail

--- other_linux/cronmail.sh
+++ other_linux/cronmail.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+# symlink /usr/sbin/sendmail to this file
+
+myself=${BASH_SOURCE[0]}
+if [ -h "$myself" ]; then
+	myself=$(readlink "$myself")
+fi
+cd "$( dirname "$myself" )"
 ./cronmail.py --from "venatorstorage@gmail.com" --to "Alessio Bianchi <venator85@gmail.com>" | msmtp -t 
