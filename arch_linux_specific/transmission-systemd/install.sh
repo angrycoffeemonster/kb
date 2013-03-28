3cc1d613819d056ab42e35816f67742ad2dfe2a2
Alessio Bianchi <venator85@gmail.com>
2013-03-28 19:57:14 +0100

    fixed transmission install script

--- arch_linux_specific/transmission-systemd/install.sh
+++ arch_linux_specific/transmission-systemd/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo systemctl stop transmission.service
 sudo systemctl disable transmission.service
-sudo systemctl enable $(PWD)/transmission.service
+sudo systemctl enable $(pwd)/transmission.service
 sudo rm -rf /run/transmission
 sudo cp transmission.conf /usr/lib/tmpfiles.d/
 sudo systemd-tmpfiles --create /usr/lib/tmpfiles.d/transmission.conf
